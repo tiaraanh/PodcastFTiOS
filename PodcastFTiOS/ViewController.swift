fdd481d2492dcf3758e2f85de1cd54c73cbf772f
Tiara <tiaranurulh@gmail.com>
2022-11-10 18:52:16 +0700

    Initial Commit

---
+++ PodcastFTiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastFTiOS
+//
+//  Created by Tiara H on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
